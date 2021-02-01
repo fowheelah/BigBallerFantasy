a92c95745a2ad4b2823ccbb8506351bfd1f25074
fowheelah <55631570+fowheelah@users.noreply.github.com>
2021-02-01 16:54:06 -0700

    Create Insert.sql

---
+++ Insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO athlete (athfirstname, athlastname, teamname)
+VALUES ('Ja', 'Morant', 'Grizzlies')
+
+INSERT INTO athlete (athfirstname, athlastname, teamname)
+VALUES ('Jerami', 'Grant', 'Pistons')
+
+INSERT INTO athlete (athfirstname, athlastname, teamname)
+VALUES ('Jaren', 'Jackson Jr.', 'Grizzlies')
+
+INSERT INTO athlete (athfirstname, athlastname, teamname)
+VALUES ('Blake', 'Griffen', 'Pistons')
+
+INSERT INTO stats (ppg, apg, rpg, spg, bpg, athleteid)
+VALUES (22, 7, 4, 2, 2, 1)
+
+INSERT INTO stats (ppg, apg, rpg, spg, bpg, athleteid)
+VALUES (24, 4, 6, 2, 2, 2)
+
+INSERT INTO thread (threadname)
+VALUES ('NBA history')
+
+INSERT INTO usercomments (commentsubject, commenttext, commentdate, commenttime, threadid)
+VALUES ('Grizzlies are greate', 'The grizz are so awesome!', '2021-2-1', '1700', '1')
+
+INSERT INTO userinfo (firstname, lastname, username, adminstatus)
+VALUES ('Sam', 'Judd', 'juddsam', 1)
+
+INSERT INTO userinfo (firstname, lastname, username, adminstatus)
+VALUES ('Mason', 'Wheeler', 'fowheelah', 1)
+
+INSERT INTO userinfo (firstname, lastname, username, adminstatus)
+VALUES ('Claudio', 'Alfaro Carvajar', 'calfaroc', 1)
